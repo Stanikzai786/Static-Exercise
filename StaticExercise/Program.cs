da25d2a9499ca340c51ac5e80e8ed661287aecc0
Ahmad Stanikzai <Shekibstanikzai.2016@gmail.com>
2022-12-13 12:44:10 -0800

    done

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,8 @@
-﻿namespace StaticExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿using StaticExercise;
+using System;
+
+Console.WriteLine($"Fahrentheit ===> Celsius: {TempConverter.FahrenheitToCelsius(32)}");
+Console.WriteLine();
+   
+Console.WriteLine($"Celsius ==> Fahrentheit : {TempConverter.CelsiusToFahrenheit(0)}");
+Console.WriteLine();
